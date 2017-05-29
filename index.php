7145af821bacdfeea226b7b2d7a37f82b838773f
MuhammedKpln <gokdeniz91@gmail.com>
2017-05-29 18:13:36 +0000

    begin

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Hangi oyun karakterisin?</title>
+      <meta property="og:url" content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html" />
+      <meta property="og:type" content="article" />
+      <meta property="og:title" content="<?php echo getOyuncu () ?>" />
+      <meta property="og:description" content="How much does culture influence creative thinking?" />
+      <meta property="og:image" content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg" />
+  </head>
+  <body>
+
+  </body>
+</html>
